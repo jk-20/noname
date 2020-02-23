22d1c834c5b6ccc756311e2714889cbcd4534bc8
jogindra_kumar <joginder9868@outlook.com>
2020-02-23 08:14:25 +0530

    modified:   admin/includes/session.php

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -2,17 +2,27 @@
 
 class Session{
 
-private $sign_in;
+private $sign_in = false;
 public $user_id;
 
 
     function __construct(){
         session_start();
+        $this->check_the_login();
     }
 
 }
 
+public function is_sign_in(){
+    return $this->$sign_in;
+}
 
+public function login($user_id){
+if($user){
+    $this->user_id = $_SESSION['user_id'] = $user->id;
+    $this->sign_in = true;
+}
+}
 
 private function check_the_login(){
     if(isset($_SESSION['user_id'])){
