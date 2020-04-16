8ac4fb2e40189b61df3bc0317d94067f2664b4e9
jogindra_kumar <joginder9868@outlook.com>
2020-04-16 09:02:29 +0530

    modified:   admin/includes/init.php 	modified:   admin/includes/paginate.php 	modified:   index.php

--- admin/includes/paginate.php
+++ admin/includes/paginate.php
@@ -4,39 +4,39 @@ class Paginate{
 
 
     public $current_page;
-    public $item_per_page;
-    public $item_total_count;
+    public $items_per_page;
+    public $items_total_count;
 
 
-    public function __construct($current_page=1, $items_per_page=4, $item_total_count=0){
+    public function __construct($page=1, $items_per_page=4, $items_total_count=0){
 
-        $this->current_page =(int)$current_page;
+        $this->current_page =(int)$page;
         $this->items_per_page =(int)$items_per_page;
-        $this->item_total_count =(int)$item_total_count;
+        $this->items_total_count =(int)$items_total_count;
 
     }
 
 
     public function next(){
 
-        return $this->current_page + 1;
+        return $this->current_page +1;
     }
 
     public function previous(){
 
-        return $this->current_page - 1;
+        return $this->current_page -1;
     }
 
 
     public function page_total(){
-        return ceil($this->item_total_count/$this->item_per_page);
+        return ceil($this->items_total_count/$this->items_per_page);
     }
 
 
 
     public function has_previous(){
 
-        return $this->previous() >= 1 ? true : false;
+        return $this->previous() >=1 ? true : false;
     }
     public function has_next(){
 
@@ -45,7 +45,7 @@ class Paginate{
 
     public function offset(){
 
-        return ($this->current_page - 1) * $this->item_per_page;
+        return ($this->current_page -1) * $this->items_per_page;
     }
 
 
--- index.php
+++ index.php
@@ -1,8 +1,15 @@
         <?php include("admin/includes/init.php");
         include("includes/header.php");
-        $photos = Photo::find_all();
+        // $photos = Photo::find_all();
+        $page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
+        $items_per_page = 4;
+        $items_total_count = Photo::count_all();
 
-
+        $paginate = new Paginate($page,$items_per_page,$items_total_count);
+        $sql = "SELECT * FROM photo ";
+        $sql .= " LIMIT {$items_per_page} ";
+        $sql .= " OFFSET {$paginate->offset()}";
+        $photos = Photo::find_this_query($sql);
         ?>
 
 
