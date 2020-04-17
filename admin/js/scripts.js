9e426bbd5e5e5323a765f5d2d9a2e14ce4fcc68a
jogindra_kumar <joginder9868@outlook.com>
2020-04-17 20:12:16 +0530

    modified:   admin/js/scripts.js

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
 
-    
+
+   
+
+    $('.modal_thumbnails').click(function(){
+        
+    $('#set_user_image').prop('disabled',false);
+
+    });
+
     tinymce.init({selector:'textarea'});
-
-
 });
