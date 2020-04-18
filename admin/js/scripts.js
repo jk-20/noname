31b90fa550ee17ff1c56169b17feecbc61e562e7
jogindra_kumar <joginder9868@outlook.com>
2020-04-18 06:03:19 +0530

    modified:   admin/js/scripts.js

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -3,16 +3,24 @@ $(document).ready(function(){
 var user_href;
 var user_href_splitted;
 var user_id;
+var image_src;
+var image_src_splitted;
+var image_id;
    
 
-    $(".modal_thumbnails").click(function(){
-        
-    $("#set_user_image").prop('disabled',false);
+$(".modal_thumbnails").click(function(){
+    
+$("#set_user_image").prop('disabled',false);
 
-    user_href = $("#user-id").prop('href');
-    user_href_splitted = user_href.split("=");
-    user_id = user_href_splitted[user_href_splitted.length -1];
-    alert(user_id );
+user_href = $("#user-id").prop('href');
+user_href_splitted = user_href.split("=");
+user_id = user_href_splitted[user_href_splitted.length -1];
+
+image_src = $(this).prop("src");
+image_src_splitted = image_src.split("/");
+image_id = image_src_splitted[image_src_splitted.length -1];
+
+alert(image_id);
 
     });
 
