2a1f37d18a6ef88d4b86542b465aecfb27bccdbb
jogindra_kumar <joginder9868@outlook.com>
2020-04-20 21:31:15 +0530

    modified:   admin/includes/ajax_code.php 	modified:   admin/includes/user.php 	modified:   admin/js/scripts.js

--- admin/includes/ajax_code.php
+++ admin/includes/ajax_code.php
@@ -1,14 +1,48 @@
-<?php require_once("init.php"); 
- $user = new User();
-if(isset($_POST['image_name'])){
-    
-   $user->ajax_save_user_image($_POST['image_name'],$_POST['user_id']) ;
-}
 
-if(isset($_POST['photo_id'])){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php 
+// require_once("init.php"); 
+//  $user = new User();
+// if(isset($_POST['image_name'])){
     
-   echo "its work";
-}
+//    $user->ajax_save_user_image($_POST['image_name'],$_POST['user_id']) ;
+// }
+
+// if(isset($_POST['photo_id'])){
+    
+//    echo "its work";
+// }
 
 
 
